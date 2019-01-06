2e9c97f27d1a9fe2873ebfd72720eee30d5f85a8
Nicola <nicola.dalmaso@outlook.com>
2019-01-06 18:10:56 +0100

    String parse implemented.

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ const ajv = new Ajv({
 const rawValidate = ajv.compile(jsonSchema);
 
 class ValidationResult {
-	constructor(status, errors) {
+	constructor(meta, status, errors) {
+		this.meta = meta;
 		this.ok = status;
 		this.errors = errors;
 	}
 }
 
-function validate(obj) {
-	return new ValidationResult(rawValidate(obj), rawValidate.errors);
+function validate(obj, meta = {src: undefined}) {
+	if (typeof obj === 'string') {
+		try {
+			const parsed = JSON.parse(obj);
+			return new ValidationResult(meta, rawValidate(parsed), rawValidate.errors);
+		} catch (error) {
+			// TODO
+			// File is not a valid JSON:
+			// last chance is to try to guess if it's a path and then try to work with that
+
+			// TODO: use something to give the user/client a more accessible error
+			return new ValidationResult(meta, false, ['Specified argument was not a valid JSON literal']);
+		}
+	}
+	return new ValidationResult(meta, rawValidate(obj), rawValidate.errors);
 }
 
 module.exports = validate;
