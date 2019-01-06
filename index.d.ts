1f1cb807040edb38cced21433a855d46adb6b603
Nicola <nicola.dalmaso@outlook.com>
2019-01-06 18:11:53 +0100

    Updated Typescript Definition.
    Might need more work on this.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,34 @@
-interface ValidationResult {
-	status: boolean;
-	errors: ajv.ErrorObject[];
-	ok: () => boolean;
-}
+/**
+ * Metadata object interface.
+ */
+interface ObjectSource {
+	/**
+	 * Optional source of the object being validated (best example would be a file path).
+	 */
+	src?: string;
+};
 
-export function validate(obj: object): ValidationResult;
+/**
+ * Validation results.
+ */
+interface ValidationResult {
+	/**
+	 * If true, the object is a valid Monika After Story piano song. False otherwise.
+	 */
+	ok: boolean;
+	/**
+	 * Array of errors caught by the validator. If ok === true than this is an empty array.
+	 */
+	errors: ajv.ErrorObject[];
+	/**
+	 * Metadata object.
+	 */
+	meta: ObjectSource;
+};
+
+/**
+ * Perform JSON schema validation to the specified object.
+ * @param obj Required object to check. If of type string, JSON.parse will be called.
+ * @param meta Optional object, useful for "signing" the particular validation with an id.
+ */
+export default function validate(obj: object|string, meta?: ObjectSource): ValidationResult;
